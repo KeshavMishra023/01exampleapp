bad5baa6228b2eb485f956b7669b44252807de8c
KeshavMishra023 <keshavmishra0024@gmail.con>
2025-02-17 14:04:29 +0530

    this is a second day working create a real time updation time

--- src/projects/Todo/Todo.jsx
+++ src/projects/Todo/Todo.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FaCheck } from "react-icons/fa6";
+
+import { MdOutlineDeleteForever } from "react-icons/md";
 
 export const Todo = () => {
     const [inputValue, setInputValue] =useState("");
+    const [dateTime, setDateTime] = useState("");
 
     const [task, setTask] = useState([]);
 
@@ -17,19 +21,51 @@ export const Todo = () => {
         if(task.includes(inputValue)){
             setInputValue("");
             return;
-        }
+        };
 
         setTask((prevTask) => [...prevTask, inputValue]);
         
         setInputValue("");
     };
-    return(
-    <section className="flex items-center flex-col" >
         
-        <header className="py-[3%]">
+    // Todo date and time
+
+    useEffect(() => {
+        const interval =   setInterval(()=> {
+            const now = new Date();
+            const formattedDated = now.toLocaleDateString();  /*it is a method to call current date */
+            const formattedTime = now.toLocaleTimeString(); /* it is a method to call and print the current time*/
+            
+            setDateTime(`${formattedDated} - ${formattedTime}`);
+        
+           }, 1000);
+
+           return() => clearInterval(interval);
+    }, [])
+
+    // handleDelteTodo 
+
+    const handleDelteTodo = (value) => {
+        console.log(task);
+        console.log(value);
+        const updatedTask = task.filter((curTask) => curTask !== value);
+        setTask(updatedTask);
+    };
+
+
+    // handleClearAll  clear all functionallity
+    const handleClearAll = () => {
+        setTask([]);
+    };
+   
+    return(
+    <section className="flex items-center flex-col gap-3" >
+        
+        <header className="py-[20px] pt-[4rem] text-center">
             <h1 className="text-5xl">
                 Todo list
             </h1>
+            <h2 className="text-2xl mt-4.5"> {dateTime} </h2>
         </header>
         <section>
             <form className="flex items-center gap-0" onSubmit={handleFormSumbit}>
@@ -52,15 +88,33 @@ export const Todo = () => {
             </form>
         </section>
         <section>
-            <ul>
+            <ul className="flex flex-col gap-3 text-2xl mt-2.5">
                 {
-                    task.map((curTask) => {
-                        return <li key={index} >
-                            <span></span>
+                    task.map((curTask, index) => {
+                        return <li key={index} className="py-[12px] rounded-4xl px-[20px] bg-gray-300 flex items-center  gap-[5rem] " >
+                            <span>{curTask}</span>
+                            <div className="flex gap-3">
+                                <button 
+                                className="  rounded-3xl p-[4px] bg-green-400 text-white" >
+                                <FaCheck />
+                                </button>
+                                <button 
+                                className=" p-[4px] bg-red-500 text-white rounded-3xl " 
+                                onClick={()=>handleDelteTodo(curTask)}>
+                                <MdOutlineDeleteForever />
+                                </button>
+                            </div>
                         </li> ;
                     })
                 }
             </ul>
+        </section>
+        <section>
+            <button 
+            className="py-[10px] px-[35px] bg-red-500 rounded-2xl hover:bg-red-800"
+            onClick={handleClearAll}  >
+                Clear all
+            </button>
         </section>
     </section>);
 };
